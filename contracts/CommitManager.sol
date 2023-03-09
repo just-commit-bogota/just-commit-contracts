9f8871aeda6406f8babd2ec5c849878229dee01b
Daniel Belfort <daniel@belfort.com>
2023-03-09 14:54:49 -0400

    cannot attest to yourself fix

--- contracts/CommitManager.sol
+++ contracts/CommitManager.sol
@@ -17,10 +17,11 @@ contract CommitPortal is Ownable {
   struct Commit {
     uint256 id; // the totalCommits proxy (for now)
     address commitFrom;
-    address commitTo;
+    address[] commitTo;
     uint256 createdAt;
-    uint256 validThrough;
-    uint256 judgeDeadline; // validThrough + 24 hours
+    uint256 startsAt;
+    uint256 endsAt;
+    uint256 judgeDeadline; // endsAt + 24 hours
     uint256 stakeAmount; // native token
     string message;
     string ipfsHash;
@@ -28,16 +29,17 @@ contract CommitPortal is Ownable {
     bool commitProved;
     bool commitJudged;
     bool isApproved;
-    bool isChallenge;
+    bool isSolo;
   }
 
   // "NewCommit" event
   event NewCommit(
     uint256 id,
     address indexed commitFrom,
-    address indexed commitTo,
+    address[] indexed commitTo,
     uint256 createdAt,
-    uint256 validThrough,
+    uint256 startsAt,
+    uint256 endsAt,
     uint256 judgeDeadline, 
     uint256 stakeAmount,
     string message,
@@ -46,7 +48,7 @@ contract CommitPortal is Ownable {
     bool commitProved,
     bool commitJudged,
     bool isApproved,
-    bool isChallenge
+    bool isSolo
   );
 
   // "NewProve" event
@@ -68,54 +70,45 @@ contract CommitPortal is Ownable {
     totalCommits = 0;
   }
 
-  // utils
-  function hasPendingCommits() public view returns (bool) {
-    for (uint i = 0; i < commits.length; i++) {
-      if (commits[i].commitFrom == msg.sender && 
-          !commits[i].commitProved &&
-          commits[i].validThrough > block.timestamp) {
-        return true;
-      }
-    }
-    return false;
+  // a Getter for the Commit array
+  function getAllCommits() public view returns (Commit[] memory) {
+    return commits;
+  }
+
+  // a Getter for the totalCommits integer
+  function getTotalCommits() public view returns (uint256) {
+    return totalCommits;
   }
 
   // (1) CREATE -> (2) PROVE -> (3) JUDGE
 
-  // create a commit
-  function createCommit(string memory _message, address commitTo, uint256 validThrough, uint256 commitsToCreate) external payable {
-    require(commitTo != msg.sender, "Cannot commit to yourself");
+  // (1) CREATE
+  function createCommit(string memory _message, address[] memory commitTo, uint256 startsAt, uint256 endsAt, bool isSolo) external payable {
+    // checks
     require(msg.value >= 0, "Stake amount must be positive");
-    require(validThrough > block.timestamp, "The commitment can't be in the past");
-    require(commitsToCreate >= 1, "You have to create at least 1 commitment");
-    require(commitsToCreate == 30 || commitsToCreate == 60, "You can only create 30 or 60 commitments at a time");
-    require(!hasPendingCommits(), "You have at least one pending commitment");
-
-    bool isChallenge;
-
-    if (commitsToCreate > 1) {
-      isChallenge = true;
-      validThrough = block.timestamp + (86400 * 1000); // 24 hours
-    } else {
-      isChallenge = false;
+    require(endsAt > block.timestamp, "The commitment can't be in the past");
+    for (uint256 i = 0; i < commitTo.length; i++) {
+      require(commitTo[i] != msg.sender, "Cannot attest yourself");
     }
 
-    for (uint256 i = 0; i < commitsToCreate; i++) {
-      Commit memory newCommit = Commit(totalCommits, msg.sender, commitTo, block.timestamp, validThrough, validThrough + ((i+1)*86400), msg.value / commitsToCreate, _message, "", "", false, false, false, isChallenge);
-      commits.push(newCommit);
-      totalCommits += 1;
+    // create
+    Commit memory newCommit = Commit(
+      totalCommits, msg.sender, commitTo, block.timestamp, startsAt, endsAt, endsAt + (86400 * 1000),
+      msg.value, _message, "", "", false, false, false, isSolo
+    );
 
-      emit NewCommit(totalCommits, msg.sender, commitTo, block.timestamp, validThrough, validThrough + ((i+1)*86400), msg.value / commitsToCreate, _message, "", "", false, false, false, isChallenge);
-    }
+    // update
+    commits.push(newCommit);
+    totalCommits += 1;
   }
 
-  // prove a commit
+  // (2) PROVE
   function proveCommit(uint256 commitId, string memory _ipfsHash, string memory _filename) external {
     Commit storage commit = commits[commitId];
 
     require(commit.commitFrom == msg.sender, "You are not the creator of this commit");
     require(commit.commitProved == false, "This commit has already been proved");
-    require(commit.validThrough > block.timestamp, "This commit has expired");
+    require(commit.endsAt > block.timestamp, "This commit has expired");
 
     commit.commitProved = true;
 
@@ -125,11 +118,11 @@ contract CommitPortal is Ownable {
     emit NewProve(commitId, _ipfsHash, _filename, block.timestamp);
   }
 
-  // judge a commit
+  // (3) JUDGE
   function judgeCommit(uint256 commitId, bool _isApproved) external { 
     Commit storage commit = commits[commitId];
 
-    require(commit.commitTo == msg.sender, "You are not the judge of this commit");
+    // require(!Address.contains(msg.sender, commitTo) "You are not the judge of this commit");
     require(commit.judgeDeadline > block.timestamp, "The judge deadline has expired");
     require(commit.commitJudged == false, "Commit has already been judged");
     require(bytes(commit.ipfsHash).length != 0, "Proof must be submitted before you can judge");
@@ -137,22 +130,11 @@ contract CommitPortal is Ownable {
     commit.commitJudged = true;
     commit.isApproved = _isApproved;
 
-    // contract doesn't handle payment logic for challenge commitments (for now)
-    if (_isApproved && !commit.isChallenge) {
+    if (_isApproved) {
       payable(commit.commitFrom).transfer(commit.stakeAmount);
     }
 
     emit NewJudge(commitId, _isApproved, block.timestamp);
-  }
-
-  // a Getter for the Commit array
-  function getAllCommits() public view returns (Commit[] memory) {
-    return commits;
-  }
-
-  // a Getter for the totalCommits integer
-  function getTotalCommits() public view returns (uint256) {
-    return totalCommits;
   }
 
 }
